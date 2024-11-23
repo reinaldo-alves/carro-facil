0cd2fddc1762a45c002208777f80cbc5af407aab
reinaldo-alves <reinaldoasjr8@gmail.com>
2024-11-22 22:23:17 -0300

    node I module complete

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const http = require('http');
 const Utilitario = require('./models/utilitario');
 
-var fusca = new Utilitario('ABC123', 1993, 'bege', 'Fusca', '60 mil', '115', 'Muito bom', 4, '10L', '15km/L');
-
-
-
-
-
-
 const requestListener = (req, res) => {
-    res.writeHead(200, {'Content-type':'text/plain'});
-    res.end('Olá mundo');
+    const { method, url } = req;
+    if(method === 'POST' && url === '/carro/construir') {
+        let body = new Utilitario('ABC123', 1993, 'bege', 'Fusca', '60 mil', '115', 'Muito bom', 4, '10L', '15km/L');
+        req.on('data', chunk => {
+            body = chunk.toString();
+        })
+        req.on('end', () => {
+            res.writeHead(200, {'Content-type':'application/json'});
+            res.end(JSON.stringify(body));
+        })
+    }
 }
 
 const server = http.createServer(requestListener);
 
 const port = 5000;
+const ip = '127.0.0.1'
 
-server.listen(port, () => {
-    console.info(`Servidor está rodando na porta ${port}`)
-    console.log(fusca);
+server.listen(port, ip, () => {
+    console.log(`Servidor executando na porta ${port}`);
 })
